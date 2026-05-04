130dc0c680d44c230c697fc59a441a5b29c25c9f
NITINAIP <nitinai.kan@gmail.com>
2026-05-04 12:08:05 +0700

    fix unit tests

--- tb.cronjob.template/tests/ProgramTests.cs
+++ tb.cronjob.template/tests/ProgramTests.cs
@@ -1,24 +1,34 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 using tb.cronjob.template.Services;
-
-
 namespace tb.cronjob.template.Tests;
 
 public class ProgramTests : IClassFixture<WebApplicationFactory<Program>>
 {
 
-    private readonly IJobRunner service;
+    private IJobRunner service;
+    private readonly WebApplicationFactory<Program> _factory;
     public ProgramTests(WebApplicationFactory<Program> factory)
     {
-        var scope = factory.Services.CreateScope();
-        service = scope.ServiceProvider.GetRequiredService<IJobRunner>();
+        _factory = factory.WithWebHostBuilder(builder =>
+       {
+           builder.UseEnvironment("Development");
+           builder.ConfigureTestServices(services =>
+           {
+               // Replace real DB context with mock to avoid DB connection during tests
+               services.AddScoped<IJobRunner, JobRunner>();
+           });
+       });
+
     }
 
     [Fact]
     public void Program_Should_Initializer()
     {
-
+        using var scope = _factory.Services.CreateScope();
+        service = scope.ServiceProvider.GetRequiredService<IJobRunner>();
         // Assert
         Assert.NotNull(service);
     }
